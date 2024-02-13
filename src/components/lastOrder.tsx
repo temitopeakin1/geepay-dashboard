f59f51605e0f666a80598eacac3a717fb8c578d1
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-02-13 20:15:31 +0100

    update

--- src/components/lastOrder.tsx
+++ src/components/lastOrder.tsx
@@ -16,10 +16,10 @@ const LastOrder = () => {
       <div className="mt-4 text-sm sm:text-base overflow-x-scroll sm:overflow-x-visible scrollbar pb-5 px-5">
         <div className="flex text-[#9CA4AB] w-[700px] sm:w-auto">
           <span className="flex-[1.5]">Name</span>
-          <span className="flex-[1.5]">Date</span>
-          <span className="flex-[1.5]">Amount</span>
-          <span className="flex-[1.5]">Status</span>
-          <span className="flex-[1.5]">Invoice</span>
+          <span className="flex-[1]">Date</span>
+          <span className="flex-[1]">Amount</span>
+          <span className="flex-[1]">Status</span>
+          <span className="flex-[0.5]">Invoice</span>
         </div>
         <div className="mt-5 border-t dark:border-gray-700 border-[#EDF2F6] divide-y dark:divide-gray-700 divide-[#EDF2F6] w-[700px] sm:w-auto">
           {ordersData.map((order, index) => (
--- src/components/orderBox.tsx
+++ src/components/orderBox.tsx
@@ -1,9 +1,33 @@
+import { ordersData } from '@/dummy';
+import { DownloadIcon } from '@/icons'
 import React from 'react'
 
-const OrderBox = () => {
+const OrderBox = ({ order}: { order: (typeof ordersData)[0]}) => {
+    const { amount, date, img, name, status } = order;
+
   return (
-    <div>
-      
+    <div className="py-4 flex items-center gap-2">
+      <div className="flex gap-2.5 items-center flex-[1.5]">
+      <image alt={name} src={img} height={32} width={32} />
+        <span className="dark:text-gray-400 text-secondary font-medium">
+          {name}
+        </span>
+      </div>
+      <span className="flex-[1] text-[#737373]">{date}</span>
+      <span className="dark:text-gray-300 text-[#0D062D] font-medium flex-[1]">
+        ${amount}
+      </span>
+      <span
+        className={`capitalize flex-[1] ${
+          status === "paid" ? "text-primary" : "text-error"
+        }`}
+      >
+        {status}
+      </span>
+      <button className="flex flex-[0.5] gap-1.5 items-center text-xs sm:text-sm">
+        <DownloadIcon />
+        <span>View</span>
+      </button>
     </div>
   )
 }
